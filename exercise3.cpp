ea496ec509562f0d1de3d8b3cd4cc3de4ff53406
LiShanglin <LiShanglin@DESKTOP-0ACGH0P>
2021-01-24 22:55:46 +0800

    添加项目文件。

---
+++ exercise3.cpp
@@ -0,0 +1,12 @@
+﻿#include<stdio.h>
+int main(void)
+{
+	int cows, legs;
+	printf("How many legs did you want the cow have:");
+	scanf_s("%d", &legs);
+	cows = legs / 4;
+	printf("That implies there are %d cows.\n", cows);
+
+	return 0;
+
+}
